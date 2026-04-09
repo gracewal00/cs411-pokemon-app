c987e906298fe36ee70ad112cb47deec41e0c2e1
rafamercado <rafaelmercado@csu.fullerton.edu>
2026-04-08 17:44:00 -0700

    Initial Commit

---
+++ Pokemon-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pokemon-app
+//
+//  Created by rafa mercado on 4/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pokemon-app/Pokemon_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokemon_appApp.swift
+//  Pokemon-app
+//
+//  Created by rafa mercado on 4/8/26.
+//
+
+import SwiftUI
+
+@main
+struct Pokemon_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
