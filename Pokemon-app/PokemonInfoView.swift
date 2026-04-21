23c2508c9b27419aa46032265eb82dc319a31b4b
Gilbert <GilbertC23@csu.fullerton.edu>
2026-04-20 18:53:33 -0700

    setup of data models and view structure

--- Pokemon-app/PokemonInfoView.swift
+++ Pokemon-app/PokemonInfoView.swift
@@ -2,17 +2,82 @@
 //  PokemonInfoView.swift
 //  Pokemon-app
 //
-//  Created by csuftitan on 4/20/26.
+//  Created by Gilbert Cervantes
 //
 
 import SwiftUI
 
 struct PokemonInfoView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+    // This get's our global data variables/attributes so we can save Pokemon to it
+    @EnvironmentObject var teamManager: TeamManager
 
-#Preview {
-    PokemonInfoView()
+    // The name of the Pokémon the user typed in
+    let pokemonName: String
+    
+    // set variables/attributes to keep track or scan what is actively running or executed
+    @State private var pokemon: PokemonDetail?
+    @State private var isLoading = true
+    @State private var errorMessage = ""
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            // just a progress or buffer bar for the page to load and show user
+            if isLoading {
+                ProgressView("Searching the tall grass...")
+            }
+            // in this case, if the user types a false/fake name it'll send an error message
+            else if !errorMessage.isEmpty {
+                Text("Oops! " + errorMessage)
+                    .foregroundColor(.red)
+            }
+            // If/when we found the Pokemon it will show the corresponding information
+            else if let pokemon = pokemon {
+                Text(pokemon.name.capitalized)
+                    .font(.largeTitle)
+                    .bold()
+                
+                // get's the Pokemon's picture from the internet/network
+                if let spriteUrl = pokemon.sprites.frontDefault, let url = URL(string: spriteUrl) {
+                    AsyncImage(url: url) { image in
+                        image.resizable()
+                            .scaledToFit()
+                            .frame(width: 150, height: 150)
+                    } placeholder: {
+                        ProgressView() // Spinner while the image loads
+                    }
+                }
+                
+                // Just a button to save this Pokémon to our team
+                Button {
+                    // Create a team member and save it
+                    let newTeammate = TeamMember(id: pokemon.id, name: pokemon.name, spriteURL: pokemon.sprites.frontDefault)
+                    teamManager.addToTeam(pokemon: newTeammate)
+                } label: {
+                    Text("Add to Team")
+                        .font(.headline)
+                        .padding()
+                        .background(Color.green)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+            }
+        }
+        .navigationTitle("Pokédex Data")
+        // Condintionally, when this desired screen is called to open it then runs our internet fetching function below
+        .task {
+            await getPokemonData()
+        }
+    }
+    
+    // This function will allow us to scan through our NetworkManager and retrieves the actual running data
+    func getPokemonData() async {
+        do {
+            let data = try await NetworkManager().fetchPokemon(name: pokemonName)
+            pokemon = data
+            isLoading = false
+        } catch {
+            errorMessage = "Could not find that Pokémon."
+            isLoading = false
+        }
+    }
 }
--- Pokemon-app/PokemonModels.swift
+++ Pokemon-app/PokemonModels.swift
@@ -2,17 +2,31 @@
 //  PokemonModels.swift
 //  Pokemon-app
 //
-//  Created by csuftitan on 4/20/26.
+//  Created by Gilbert Cervantes.
 //
 
-import SwiftUI
+import Foundation
 
-struct PokemonModels: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+// Note, file is to handle the: Data Models
+// Moreover, the data structure is referening apple's PetTricks learning module
+
+// This collects and handles the data from the internet's raw JSON into a format that our app can handle/understand
+struct PokemonData: Codable, Identifiable {
+    let id: Int
+    let name: String
+    let sprites: Sprites
 }
 
-#Preview {
-    PokemonModels()
+// Note, handles image etc,. : Image Data
+// This scans/handles for the image or URLs from the API
+struct Sprites: Codable {
+    let frontDefault: String?
+    
+    
+    // to consider that the web's API may call or handle the function as a different varaible such as: front_default. Whereas we have seen that Swift typically is camelCase.
+    
+    // This way we can use CodingKeys which acts the translator for both programs
+    enum CodingKeys: String, CodingKey {
+        case frontDefault = "front_default"
+    }
 }
