8e3278655b92bdfc3e7ad84b512fbc6e1768e684
Gilbert <GilbertC23@csu.fullerton.edu>
2026-04-20 18:08:49 -0700

    Adding new models and prepping Network/Team managers

---
+++ Pokemon-app/PokemonInfoView.swift
@@ -0,0 +1,18 @@
+//
+//  PokemonInfoView.swift
+//  Pokemon-app
+//
+//  Created by csuftitan on 4/20/26.
+//
+
+import SwiftUI
+
+struct PokemonInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PokemonInfoView()
+}
---
+++ Pokemon-app/PokemonModels.swift
@@ -0,0 +1,18 @@
+//
+//  PokemonModels.swift
+//  Pokemon-app
+//
+//  Created by csuftitan on 4/20/26.
+//
+
+import SwiftUI
+
+struct PokemonModels: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PokemonModels()
+}
